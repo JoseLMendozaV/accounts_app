acae01e441939ced64fda258a427f75a6d7941b9
Jose Mendoza <sagaka1g@gmail.com>
2021-08-25 00:17:01 +0100

    Added Account service Implementation

---
+++ src/main/java/com/account/accounts_app/service/AccountServiceImpl.java
@@ -0,0 +1,10 @@
+package com.account.accounts_app.service;
+
+import com.account.accounts_app.data.models.Account;
+import com.account.accounts_app.data.payloads.request.AccountRequest;
+import com.account.accounts_app.data.payloads.response.AccountResponse;
+import com.account.accounts_app.data.repository.AccountRepository;
+
+
+public class AccountServiceImpl {
+}
