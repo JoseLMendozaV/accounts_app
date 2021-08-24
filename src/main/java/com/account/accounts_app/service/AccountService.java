c1e1323555af3dbd1e5ebbb24494fd23b05df4e0
Jose Mendoza <sagaka1g@gmail.com>
2021-08-24 23:57:13 +0100

    Added Account Services

---
+++ src/main/java/com/account/accounts_app/service/AccountService.java
@@ -0,0 +1,18 @@
+package com.account.accounts_app.service;
+
+import com.account.accounts_app.data.models.Account;
+import com.account.accounts_app.data.payloads.request.AccountRequest;
+import com.account.accounts_app.data.payloads.response.AccountResponse;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public interface AccountService {
+    AccountResponse CreateAccount(AccountRequest accountRequest);
+    Optional<Account> updateAccount (Integer accountid, AccountRequest accountRequest);
+    void deleteAccount(Integer accountid);
+    Account getSingleAccount(Integer accountid);
+    List<Account> getAllAccount();
+}
