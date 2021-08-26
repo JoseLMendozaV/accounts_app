45c3f268988dd28e815713d14e18f7232dd4d17e
Jose Mendoza <sagaka1g@gmail.com>
2021-08-26 22:27:36 +0100

    delete the file delete interface

--- src/main/java/com/account/accounts_app/data/repository/deleteInterface.java
+++
@@ -1,7 +0,0 @@
-package com.account.accounts_app.data.repository;
-
-import com.account.accounts_app.data.models.Account;
-import org.springframework.data.repository.CrudRepository;
-
-public interface deleteInterface extends CrudRepository<Account, Integer> {
-}
